31d54aa651accf9abcc7c4438fec00cbccb3685f
Jean Bracho <89703408+jeanbrcho@users.noreply.github.com>
2024-10-25 23:19:58 -0300

    comente función changeicao

--- src/app/data.service.ts
+++ src/app/data.service.ts
@@ -9,6 +9,10 @@ export class DataService {
   private icaoSource = new BehaviorSubject<string | null>(null);
   currentIcao = this.icaoSource.asObservable();
 
+  /**
+   * @function changeIcao
+   * @description facilita el envío del ICAO entre FavoritesComponent y HomeComponent
+   */
   changeIcao(icao: string) {
     this.icaoSource.next(icao);
   }
